b81eca094ac36c0991c34ab02517b07ea44b4839
Jack Weilage <jack@weilage.dev>
2023-11-05 11:36:36 -0800

    Add simple example maps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,77 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Control from '$lib/components/Control.svelte'
+	import Marker from '$lib/components/Marker.svelte'
+	import Popup from '$lib/components/Popup.svelte'
+	import { Layer, Map, Source } from '$lib/index.js'
+	import { NavigationControl, ScaleControl } from 'mapbox-gl'
+	import CityPopup from './CityPopup.svelte'
+</script>
+
+<div class="maps">
+	<div class="map">
+		<h2>Marker + Popup</h2>
+
+		<Map
+			style="height: 80vh"
+			map={{
+				accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
+				style: 'mapbox://styles/mapbox/streets-v12',
+				center: [12.550343, 55.665957],
+				zoom: 8,
+				attributionControl: false,
+			}}
+		>
+			<Control position="bottom-left" control={new ScaleControl()} />
+			<Control position="bottom-right" control={new NavigationControl()} />
+			<Marker lngLat={[12.550343, 55.665957]}>
+				<Popup>Blue marker popup</Popup>
+			</Marker>
+			<Marker lngLat={[12.65147, 55.608166]} marker={{ color: 'black', rotation: 45 }}>
+				<Popup>Black tilted marker popup</Popup>
+			</Marker>
+		</Map>
+	</div>
+	<div class="map">
+		<h2>Layer + Custom Popups</h2>
+
+		<Map
+			style="height: 80vh"
+			map={{
+				accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
+				style: 'mapbox://styles/mapbox/streets-v12',
+				attributionControl: false,
+			}}
+		>
+			<Source
+				id="points"
+				source={{
+					type: 'geojson',
+					data: 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_populated_places_simple.geojson',
+				}}
+			>
+				<Layer
+					layer={{
+						id: 'points',
+						type: 'circle',
+						paint: { 'circle-color': 'red' },
+					}}
+				>
+					<CityPopup />
+				</Layer>
+			</Source>
+		</Map>
+	</div>
+</div>
+
+<style>
+	.maps {
+		padding: 1rem;
+
+		display: flex;
+		gap: 1rem;
+		flex-direction: column;
+	}
+	h2 {
+		text-align: center;
+	}
+</style>
---
+++ src/routes/CityPopup.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { LayerContext, MapContext } from '$lib/context.js'
+	import type { MapboxGeoJSONFeature } from 'mapbox-gl'
+
+	import Popup from '$lib/components/Popup.svelte'
+	import { layerContextKey, mapContextKey } from '$lib/context.js'
+	import { getContext, onMount, tick } from 'svelte'
+
+	const { mapStore } = getContext<MapContext>(mapContextKey)
+	const { layerStore } = getContext<LayerContext>(layerContextKey)
+
+	interface CityProperties {
+		scalerank: number
+		name: string
+		website: string
+		natlscale: number
+		featureClass: string
+	}
+	let selectedCity:
+		| {
+				properties: CityProperties
+				coordinates: [number, number]
+		  }
+		| undefined
+
+	function onClose() {
+		selectedCity = undefined
+	}
+	async function onClick({ features }: { features?: MapboxGeoJSONFeature[] }) {
+		if (!features?.[0]?.properties || features[0].geometry.type !== 'Point') {
+			return
+		}
+
+		//TODO: This is a nasty hack that removes the previous popup before adding a new one. This should be fixed in Popup.svelte
+		await tick()
+
+		selectedCity = {
+			coordinates: features[0].geometry.coordinates as [number, number],
+			properties: features[0].properties as CityProperties,
+		}
+	}
+	onMount(() => {
+		$mapStore.on('click', $layerStore.id, onClick)
+
+		return () => {
+			$mapStore.off('click', $layerStore.id, onClick)
+		}
+	})
+</script>
+
+{#if selectedCity}
+	<Popup on:close={onClose} lngLat={selectedCity.coordinates}>
+		<h2>
+			<a href={selectedCity.properties.website}>{selectedCity.properties.name}</a>
+		</h2>
+	</Popup>
+{/if}
